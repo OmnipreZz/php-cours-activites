c6fb147b9725200de826baa0d37ff4e6082eaf8a
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-30 13:22:52 +0200

    Exo For

--- boucles/for.php
+++ boucles/for.php
@@ -111,17 +111,18 @@ echo '<br><br>';
 //Créer un tableau pour les mois de l'année et affiché tous les mois de Janvier à Décembre
 //modifier et/ou remplacer les éléments ci-dessous
 echo "8.Les mois depuis le debut de l'annee : <br>";
-$mois = [];
-for ($i = 0; $i < 0; $i) {
-    echo '';
+$mois = [Janvier, Février, Mars, Avril, Mai, Juin, Juilet, Aout, Septembre, Octobre, Novembre, Décembre];
+for ($i = 0; $i < count($mois); $i++) {
+    echo $mois[$i].'<br>';
 }
 echo '<br><br>';
 
 //Afficher les mois de la fin de l'année jusqu'au début de l'année
 //modifier et/ou remplacer les éléments ci-dessous
 echo "9.Les mois depuis la fin de l'annee : <br>";
-for ($i = 0; $i < 0; $i) {
-    echo '';
+for ($i = 0; $i < count($mois); $i++) {
+    $reverse = array_reverse($mois);
+    echo $reverse[$i].'<br>';
 }
 echo '<br><br>';
 //----------------------------------------
@@ -148,14 +149,40 @@ $college = array(
 );
 
 echo '10.Les eleves du college : <br>';
-//ajoutez votre code ici
+foreach ($college as $key => $array) {
+  echo '<br><br>'.$key.' : '.'<br><br>';
+  foreach ($array as $index => $value) {
+    foreach ($value as $name => $valeur) {
+      echo $name.' : '.$valeur.'<br>';
+    }
+  }
+}
 echo '<br><br>';
 
 //----------------------------------------
 //Afficher le nom et prénoms des élèves de ce collège
 //reprenez le tableau ci-dessus, ajoutez des éléves pour la classe de troisième et réaffichez tout
 echo '11.Les eleves du college (avec les nouveaux arrivants): <br>';
-//ajoutez votre code ici
+// array_push($college['Troisieme']['Nom'] = 'Frioli');
+// array_push($college['Troisieme']['Prenom'] = 'Geoffrey');
+// array_push($college['Troisieme']['Nom'] = 'Emmerich');
+// array_push($college['Troisieme']['Prenom'] = 'Roland');
+$newdata =  array (
+    array('Nom' => 'Frioli', 'Prenom' => 'Geoffrey'),
+    array('Nom' => 'yoyo', 'Prenom' => 'Jojo'),
+    array('Nom' => 'Rambo', 'Prenom' => 'John'),
+    array('Nom' => 'hello', 'Prenom' => 'Kitty'),
+);
+$college["Troisieme"] = $newdata;
+
+foreach ($college as $key => $array) {
+  echo '<br><br>'.$key.' : '.'<br><br>';
+  foreach ($array as $index => $value) {
+    foreach ($value as $name => $valeur) {
+      echo $name.' : '.$valeur.'<br>';
+    }
+  }
+}
 echo '<br><br>';
 
 //----------------------------------------
@@ -196,7 +223,12 @@ $videotheque = array(
 );
 
 echo '12.Mes films : <br>';
-//ajoutez votre code ici
+foreach ($videotheque as $array => $variable) {
+    echo $array.'<br>';
+  foreach ($variable as $key => $value) {
+    echo $key.' : '.$value.'<br>';
+  }
+}
 echo '<br><br>';
 
 //----------------------------------------
@@ -206,5 +238,30 @@ echo '<br><br>';
 //rajoutez un synopsis
 
 echo '13.Mes films : <br>';
-//ajoutez votre code ici
+$newdata =  array (
+      'nom' => 'La ligne Verte',
+      'date' => 2000,
+      'realisateur' => 'Franck Darabont',
+      'acteurs' => array(
+      	'Tom Hanks', 'Michael Clarke', 'Duncan', 'David Morse',
+      ),
+      'synopsis' => "Paul Edgecomb, pensionnaire centenaire d'une maison de retraite, est hanté par ses souvenirs. Gardien-chef du pénitencier de Cold Mountain en 1935, il était chargé de veiller au bon déroulement des exécutions capitales en s’efforçant d'adoucir les derniers moments des condamnés. Parmi eux se trouvait un colosse du nom de John Coffey, accusé du viol et du meurtre de deux fillettes. Intrigué par cet homme candide et timide aux dons magiques, Edgecomb va tisser avec lui des liens très forts.",
+    );
+$videotheque[] = $newdata;
+
+foreach ($videotheque as $array => $variable) {
+    echo $array.'<br>';
+  foreach ($variable as $key => $value) {
+    if ($key != acteurs) {
+    echo $key.' : '.$value.'<br>';
+    } else {
+      echo $key.' : ';
+    }
+    foreach ($value as $ke => $val) {
+      echo $val.', ';
+    }
+  }
+  echo'<br>';
+}
+
 echo '<br><br>';
